abfa4dc6b083f6720640cea487c0b3f7f21e0318
mofasa798 <f.aji2803@gmail.com>
2025-02-05 19:27:50 +0700

    tambah paragraph

--- Documents/coba-tes2/git-tes-repo-lokal/index.php
+++ Documents/coba-tes2/git-tes-repo-lokal/index.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <h1>tambah file php</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus quo asperiores, sed obcaecati nostrum corrupti, nulla, tempora explicabo facilis iure repellendus? Doloribus possimus sint natus porro ipsum mollitia vitae eum.</p>
     
 </body>
 </html>
