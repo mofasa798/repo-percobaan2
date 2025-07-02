fc81248fcaa416e490f0df127bb9cde90ff459ee
Mohamad Fikri <146539030+mofasa798@users.noreply.github.com>
2025-07-02 20:11:08 +0700

    menambahkan file untuk latihan DOM Manipulation

---
+++ script.js
@@ -0,0 +1,36 @@
+// DOM Selection
+// document.getElementById() -> element
+// const judul = document.getElementById('judul');
+// judul.style.color = 'blue';
+// judul.style.backgroundColor = 'grey';
+// judul.innerHTML = 'Yehaaha!';
+
+// document.getElementsByTagName() -> HTMLCollections
+// const p =document.getElementsByTagName('p');
+// for( let i = 0; i < p.length; i++) {
+//     p[i].style.backgroundColor = 'lightblue';
+// }
+// const h1 = document.getElementsByTagName('h1')[0];
+// h1.style.fontSize = '50px';
+
+// document.getElementsByClassName() -> HMTLCollection
+// const p1 = document.getElementsByClassName('p1')[0];
+// p1.innerHTML = 'ini diubah dari Javascript';
+
+// document.querySelector()
+// const p4 = document.querySelector('#b p');
+// p4.style.color = 'green';
+
+// const li2 = document.querySelector('section#b ul li:nth-child(2)');
+// li2.style.backgroundColor = 'orange';
+
+// document.querySelectorAll()
+// const p = document.querySelectorAll('p');
+// for(let i = 0; i<p.length; i++) {
+//     p[i].style.backgroundColor = 'lightblue';
+// }
+
+// const sectionB = document.getElementById('b');
+// const p4 = sectionB.querySelector('p');
+// p4.style.backgroundColor = 'orange';
+
---
+++ script1.js
@@ -0,0 +1,14 @@
+// const judul = document.getElementById('judul');
+// judul.innerHTML = '<em>Ohayou!!!</em>';
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = 'Begitulah';
+
+// const judul = document.querySelector('#judul');
+// judul.style.color = 'lightblue';
+// judul.style.backgroundColor = 'salmon';
+
+// const judul = document.getElementsByTagName('h1')[0];
+// const a = document.querySelector('section#a a');
+
+const p2 = document.querySelector('.p2');
